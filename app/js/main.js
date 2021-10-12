5d26c453a804e6c6a4d084bc91bd3fee0ff683bf
Xomcheg__Work <xromenkovevgenii@mail.ru>
2021-10-12 17:39:10 +0300

    Footer

---
+++ app/js/main.js
@@ -0,0 +1,157 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+    const stickersParent = document.querySelector('.calculator__stickers'),
+        resultParent = document.querySelector('.calculator__result'),
+        calculatorMainImg = document.querySelectorAll('.calculator__main-img'),
+        stickers = document.querySelectorAll('.calculator__stickers-item');
+    let resultStickers = document.querySelectorAll('.calculator__result-stick');
+    function removeClass() {
+        stickers.forEach((item) => {
+            item.classList.remove('stickers-item--active');
+        });
+    }
+    function checkPack() {
+        let pack = document.querySelectorAll('.calculator__result-stickers img'),
+            resultButtons = document.querySelectorAll('.calculator__result-buttons');
+        if (pack.length >= 5) {
+            resultButtons[0].style.display = 'block';
+        } else {
+            resultButtons[0].style.display = 'none';
+        }
+        if (pack.length >= 10) {
+            resultButtons[1].style.display = 'block';
+        } else {
+            resultButtons[1].style.display = 'none';
+        }
+        if (pack.length >= 15) {
+            resultButtons[2].style.display = 'block';
+            calculatorMainImg.forEach(item => {
+                item.style.pointerEvents ='none';
+                item.style.opacity = '.3';
+            });
+        } else {
+            resultButtons[2].style.display = 'none';
+            calculatorMainImg.forEach(item => {
+                item.style.pointerEvents ='auto';
+                item.style.opacity = '1';
+            });
+        }
+    }
+    let i = 0;
+    let arrSticks = [];
+    stickersParent.addEventListener('click', (event) => {
+        let target = event.target;
+        const parentItem = target.parentNode.parentNode;
+        removeClass();
+        if (target.classList.contains('calculator__main-img')) {
+            parentItem.classList.add('stickers-item--active');
+        }
+        if (target.classList.contains('calculator__color-img')) {
+            let colorItem = target.cloneNode(true);
+            resultStickers[i].prepend(colorItem);
+            arrSticks[i] = colorItem;
+            resultStickers[i].classList.add('result-stick--active');
+            i++;
+            checkPack();
+        }
+    });
+    resultParent.addEventListener('click', (event) => {
+        let target = event.target;
+        if (target.classList.contains('calculator__result-del')) {
+            let delStick = arrSticks.indexOf(target.previousElementSibling);
+            for (let i = 0; i < resultStickers.length; i++) {
+                let elem = resultStickers[i].children;
+                for (let i = 0; i < elem.length; i++) {
+                    if (elem[i].classList.contains('calculator__color-img')) {
+                        elem[i].remove();
+                        elem[i].parentNode.classList.remove('result-stick--active');
+                    }
+                }
+            }
+            arrSticks.splice(delStick, 1);
+            for (let i = 0, j = arrSticks.length; i < j; i++) {
+                resultStickers[i].prepend(arrSticks[i]);
+                resultStickers[i].classList.add('result-stick--active');
+            }
+            i = arrSticks.length;
+            checkPack();
+        }
+
+        let item;
+
+        if (target.classList.contains('calculator__result-cleaning')) {
+            item = target.parentNode.parentNode.previousElementSibling;
+        } else if (target.classList.contains('calculator__result-cleaning-img')) {
+            item = target.parentNode.parentNode.parentNode.previousElementSibling;
+            console.log(item);
+        }
+        let elem = item.children;
+        for (let i = 0; i < elem.length; i++) {
+            let items = elem[i].children;
+            elem[i].classList.remove('result-stick--active');
+            items[0].remove();
+        }
+        if (item.classList.contains('first__pack')) {
+            arrSticks.splice(0, 5);
+        } else if (item.classList.contains('second__pack')) {
+            arrSticks.splice(5, 5);
+        } else if (item.classList.contains('third__pack')) {
+            arrSticks.splice(10, 5);
+        }
+
+        for (let i = 0, j = arrSticks.length; i < j; i++) {
+            resultStickers[i].prepend(arrSticks[i]);
+            resultStickers[i].classList.add('result-stick--active');
+        }
+
+        for (let i = arrSticks.length; i < resultStickers.length; i++) {
+            resultStickers[i].classList.remove('result-stick--active');
+        }
+
+        i = arrSticks.length;
+
+        checkPack();
+        // if (target.classList.contains('calculator__result-cleaning')) {
+        //     if (target.classList.contains('calculator__result-cleaning-img')){
+        //         // let item
+        //     }
+        //     item = target.parentNode.parentNode.previousElementSibling;
+        //     let elem = item.children;
+        //     console.log(elem);
+        //     for (let i = 0; i < elem.length; i++) {
+        //         let items = elem[i].children;
+        //         elem[i].classList.remove('result-stick--active');
+        //         items[0].remove();
+        //     }
+        //     if (item.classList.contains('first__pack')) {
+        //         arrSticks.splice(0, 5);
+        //     } else if(item.classList.contains('second__pack')){
+        //         arrSticks.splice(5, 5);
+        //     } else if(item.classList.contains('third__pack')){
+        //         arrSticks.splice(10, 5);
+        //     }
+
+        //     for (let i = 0, j = arrSticks.length; i < j; i++) {
+        //         resultStickers[i].prepend(arrSticks[i]);
+        //         resultStickers[i].classList.add('result-stick--active');
+        //     }
+
+        //     for (let i = arrSticks.length; i < resultStickers.length; i ++) {
+        //         resultStickers[i].classList.remove('result-stick--active');
+        //     }
+        //     i = arrSticks.length;
+
+        //     checkPack();
+        // }
+    });
+
+
+
+
+
+
+
+
+
+});
